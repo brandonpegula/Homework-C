bd026342eab16011bd7f454097908ab8d7562e43
Brandon P <brandonpegula@gmail.com>
2018-01-23 15:49:38 -0500

    First Commit

---
+++ c.js
@@ -0,0 +1,38 @@
+// function Multiplier(multiply, getCurrentValue){
+// 	this.multiply = multiply;
+// 	this.getCurrentValue = getCurrentValue;
+	
+// }	
+
+// var Photo = new Multiplier
+
+// Assignment C1
+
+// class Multiplier{
+//     constructor(multiply, getCurrentValue){
+//     this.multiply = function() {
+//         var b = a * 1;
+//         lastValue = a;
+//         return b;
+//     }
+
+//     this.getCurrentValue = function() {
+//         return lastValue;
+//     }
+// }
+// }
+
+// Assignment C2
+
+class Album{
+	constructor(name, location){
+			this.name = name;
+			this.location = location;
+		}
+}
+
+
+var photo = new Album('name.jpg', 'desktop')
+
+// var test = new Multiplier(4,5);
+// console.log(test)
